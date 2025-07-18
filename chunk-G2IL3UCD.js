67c671db282b61cdf547556f7bae62e227601625
avila2601 <diegoavila2601@hotmail.com>
2025-07-18 18:23:52 -0500

    Auto-generated commit

--- chunk-G2IL3UCD.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2VMXMS7J.js";var t=[{path:"",loadComponent:()=>import("./chunk-XP2NNNDR.js").then(o=>o.MoviesComponent)},{path:":movieId",loadComponent:()=>import("./chunk-MOELNRS5.js").then(o=>o.MovieDetailsComponent)}];export{t as moviesRoutes};
