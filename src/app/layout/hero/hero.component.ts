4ce15d6aaead59f4496a70e2f50f0e044c320645
avila2601 <diegoavila2601@hotmail.com>
2025-08-21 16:08:14 -0500

    botonesHero

--- src/app/layout/hero/hero.component.ts
+++ src/app/layout/hero/hero.component.ts
@@ -1,5 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Movie } from '../../features/movies/models/movies.interface';
+import { MoviesService } from '../../features/movies/movies.service';
 
 @Component({
   selector: 'app-hero',
@@ -7,9 +9,40 @@ import { Movie } from '../../features/movies/models/movies.interface';
 })
 export class HeroComponent {
 
+  movie = input.required<Movie>();
+
+  private readonly _router = inject(Router);
+  private readonly _moviesService = inject(MoviesService);
+
   constructor() { }
 
-  movie = input.required<Movie>();
+  verTrailer(): void {
+    const movieData = this.movie();
+    this._moviesService.getMovieTrailer(movieData.id).subscribe((res) => {
+      const trailer = res.results.find((v: any) => v.site === 'YouTube' && v.type === 'Trailer');
+      if (trailer) {
+        window.open(`https://www.youtube.com/watch?v=${trailer.key}`, '_blank');
+      } else {
+        alert('No se encontró tráiler para esta película.');
+      }
+    });
+  }
+
+  masInformacion(): void {
+    const movieData = this.movie();
+
+    // Primero hacer scroll hasta el final del hero
+    const heroHeight = window.innerHeight * 0.8; // 80vh del hero
+    window.scrollTo({
+      top: heroHeight,
+      behavior: 'smooth'
+    });
+
+    // Esperar un poco para que termine el scroll y luego navegar
+    setTimeout(() => {
+      this._router.navigate(['/movies', movieData.id]);
+    }, 0); // 600ms para que termine el scroll suave
+  }
 
 
 }
